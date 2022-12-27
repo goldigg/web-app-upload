664e13e2b0d4523f47273048c106c307af9ff19f
ggoldmann-pgs <ggoldmann@pgs-soft.com>
2022-12-27 14:37:25 +0100

    Remove main fun.

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import os
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
 
 
 s3BucketName = "ggoldmann-pgs-upskill"
