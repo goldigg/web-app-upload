d86611f8253d695a6071935c8bd4007bcaa5e627
ggoldmann-pgs <ggoldmann@pgs-soft.com>
2022-12-27 13:58:04 +0100

    Restore helpers

--- helpers.py
+++ helpers.py
@@ -1,7 +1,14 @@
 # # in util/helpers.py
 
-# import boto3, botocore
+import boto3, botocore
 
-# s3 = boto3.client(
-#     "s3"
-# )
+config = Config(
+    region_name = 'eu-central-1',
+    signature_version = 'v4',
+    retries = {
+        'max_attempts': 10,
+        'mode': 'standard'
+    }
+)
+
+s3_client = boto3.client('s3', config=config)
