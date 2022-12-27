8638a714284f878f8e408219d10ebde0c1d95a0e
ggoldmann-pgs <ggoldmann@pgs-soft.com>
2022-12-27 12:49:47 +0100

    public.ecr.aws/ablachowicz-public-ecr-reg/ggoldmann_s3_web

--- helpers.py
+++ helpers.py
@@ -3,7 +3,5 @@
 import boto3, botocore
 
 s3 = boto3.client(
-    "s3",
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+    "s3"
 )
